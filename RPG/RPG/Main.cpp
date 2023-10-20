4a0230d5120d608917d4baa451e4f6a89bc8c46a
Haru <loicgiacosa@gmail.com>
2023-10-20 14:12:49 +0200

    EnemyTakesDamage()
    Les enemis prennent des dégats

--- RPG/RPG/Main.cpp
+++ RPG/RPG/Main.cpp
@@ -4,7 +4,14 @@ using namespace std;
 
 int main()
 {
-
+	
 
 	return 0;
+}
+
+void EnemyTakesDamage(int*& _enemyHealth, int _strenghtUser)
+{
+	int* _newHealth = new int(*_enemyHealth - _strenghtUser);
+	delete _enemyHealth;
+	_enemyHealth = _newHealth;
 }
